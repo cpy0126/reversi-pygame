ccb47ef70f63ce01c521a1938ce2b4f7e4983141
cpy0126 <0126morris@gmail.com>
2020-12-20 23:03:02 +0800

    test

--- agent/cpy0126.py
+++ agent/cpy0126.py
@@ -257,28 +257,163 @@ class MyAgent(BaseAgent):
         if cur_num==num :
             return bestmove
         return score
+class MyAgentR(BaseAgent):
+
+    def get_score1(self,obs):
+        weight =[[90,-60,10,10,10,10,-60,90],
+                [-60,-80,5,5,5,5,-80,-60],
+                [10,5,1,1,1,1,5,10],
+                [10,5,1,1,1,1,1,10],
+                [10,5,1,1,1,1,5,10],
+                [10,5,1,1,1,1,5,10],
+                [-60,-80,5,5,5,5,-80,-60],
+                [90,-60,10,10,10,10,-60,90]]
+        score = 0
+        for i in range(self.rows_n):
+            for j in range(self.cols_n):
+                if obs[i][j] == self.color:
+                    score += weight[i][j]
+        return score
+
+    def get_score2(self,obs):
+        num=0
+        for i in range(self.rows_n):
+                for j in range(self.cols_n):
+                    if obs[i][j] == self.color:
+                        num+=1
+        return num
     
-    def dfsS(self,obs,cur_color):
-        legal_move = self.legalMove(cur_color,copy.deepcopy(obs))
-        min_score = 1000
-        if not legal_move :
+    def trans(self,array):
+        count = 0
+        transarray=[]
+        for i in range(8):
+            transarray.append([])
+            for j in range(8):
+                transarray[i].append(array[count])
+                count+=1
+        return transarray
+
+    def legalMove(self,color,obs):
+        LegalMove=[]
+        for i in range(self.rows_n):
+            for j in range(self.cols_n):
+                cobs= self.act(i,j,color,copy.deepcopy(obs))
+                if cobs!=None:
+                    LegalMove.append((i,j))
+        
+        return LegalMove
+
+    def act(self,x,y,color,obs):
+        if obs[x][y]!=0:
             return None
-        bestmove = None
-        corner = [(0,0),(0,7),(7,0),(7,7)]
+        cobs=copy.deepcopy(obs)
+        cobs[x][y]=color
+
+        flipped = False
+        row = x
+        col = y
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if i==0 and j==0: continue
+                row=x
+                col=y
+                ready_flip = []
+                while 0<=col+j<self.cols_n and 0<=row+i<self.rows_n:
+                    row+=i
+                    col+=j
+                    if obs[row][col]==color:
+                        while ready_flip:
+                            cobs[ready_flip[0][0]][ready_flip[0][1]]=color
+                            ready_flip.pop(0)
+                            flipped=True
+                        break
+                    elif obs[row][col]==0: break
+                    else:
+                        ready_flip.append((row,col))
+        if flipped:
+            return cobs
+        else:
+            return None
+
+    def empty(self,obs):
+        count = 0
+        for i in range(self.rows_n):
+            for j in range(self.cols_n):
+                if obs[i][j] == 0:
+                    count+=1
+        return count
+
+    def step(self,reward, obs):
+        empty=self.empty(self.trans(obs))
+        if empty<14:
+            bestMove = self.dfsW2(self.trans(obs),self.color,2,2)
+        else:
+            bestMove = self.dfsW1(self.trans(obs),self.color,2,2)
+        return (self.col_offset + (bestMove[1]) * self.block_len, self.row_offset + (bestMove[0]) * self.block_len), pygame.USEREVENT
+
+    def dfsW2(self,obs,cur_color,num,cur_num):
+        legal_move= self.legalMove(cur_color,copy.deepcopy(obs))
+        max_score = -1000000 *cur_color*self.color
+        bestmove = (0,0)
+        score=0
+        if not legal_move :
+            return max_score
         for i in range(len(legal_move)):
+            if (cur_num==num) and ((legal_move[i][0]==0) or (legal_move[i][0]==7) ) and ((legal_move[i][1]==0) or (legal_move[i][1]==7)):
+                return legal_move[i]
+            if cur_num > 0 :
+                branch = self.act(legal_move[i][0],legal_move[i][1],cur_color,copy.deepcopy(obs))#put the legal move on board
+                score=self.dfsW2(branch,-cur_color,num,cur_num-1)
+            if cur_num == 0 :
+                branch = self.act(legal_move[i][0],legal_move[i][1],cur_color,copy.deepcopy(obs))#put the legal move on board
+                score=self.get_score2(branch)
+            if cur_color==self.color and score >= max_score:
+                if score > max_score :
+                    bestmove = legal_move[i]
+                elif random.randint(0,1):
+                    bestmove = legal_move[i]
+                max_score = score
+            if cur_color==-self.color and score <= max_score:
+                if score > max_score :
+                    bestmove = legal_move[i]
+                elif random.randint(0,1):
+                    bestmove = legal_move[i]
+                max_score = score
+        if cur_num==num :
+            return bestmove
+        return score
 
-            for j in range(4): #check corners
-                if legal_move[i]==corner[j]:
-                    branch = self.act(legal_move[i][0],legal_move[i][1],cur_color,copy.deepcopy(obs))
-                    return legal_move[i]
-
-            branch = self.act(legal_move[i][0],legal_move[i][1],cur_color,copy.deepcopy(obs))#put the legal move on board
-            score = len(self.legalMove(cur_color,branch))
-            if score <= min_score:
-                min_score = score
-                bestmove = legal_move[i]
-
-        return bestmove
+    def dfsW1(self,obs,cur_color,num,cur_num):
+        legal_move= self.legalMove(cur_color,copy.deepcopy(obs))
+        max_score = -1000000 *cur_color*self.color
+        bestmove = (0,0)
+        score=0
+        if not legal_move :
+            return max_score
+        for i in range(len(legal_move)):
+            if (cur_num==num) and ((legal_move[i][0]==0) or (legal_move[i][0]==7) ) and ((legal_move[i][1]==0) or (legal_move[i][1]==7)):
+                return legal_move[i]
+            if cur_num > 0 :
+                branch = self.act(legal_move[i][0],legal_move[i][1],cur_color,copy.deepcopy(obs))#put the legal move on board
+                score=self.dfsW1(branch,-cur_color,num,cur_num-1)
+            if cur_num == 0 :
+                branch = self.act(legal_move[i][0],legal_move[i][1],cur_color,copy.deepcopy(obs))#put the legal move on board
+                score=self.get_score1(branch)
+            if cur_color==self.color and score >= max_score:
+                if score > max_score :
+                    bestmove = legal_move[i]
+                elif random.randint(0,1):
+                    bestmove = legal_move[i]
+                max_score = score
+            if cur_color==-self.color and score <= max_score:
+                if score > max_score :
+                    bestmove = legal_move[i]
+                elif random.randint(0,1):
+                    bestmove = legal_move[i]
+                max_score = score
+        if cur_num==num :
+            return bestmove
+        return score
 
         
 
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import subprocess
-result = subprocess.check_output(['python3','arena.py','--agent1','cpy0126.MyAgent','--agent2','base_agent.MyAgent','--headless','--rounds','300'])
+result = subprocess.check_output(['python3','arena.py','--agent1','cpy0126.MyAgent','--agent2','cpy0126.MyAgentR','--headless','--rounds','30'])
 print(result)
-result = subprocess.check_output(['python3','arena.py','--agent2','cpy0126.MyAgent','--agent1','base_agent.MyAgent','--headless','--rounds','300'])
+result = subprocess.check_output(['python3','arena.py','--agent2','cpy0126.MyAgent','--agent1','cpy0126.MyAgentR','--headless','--rounds','30'])
 print(result)
